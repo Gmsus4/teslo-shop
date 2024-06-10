d080d0e5b8a9d97887d3b28fd466a8c79212867a
Fernando <jesusfgm45@gmail.com>
2024-06-10 12:38:07 -0600

    Pantalla carrito de compras - Parte 2

--- src/app/(shop)/cart/page.tsx
+++ src/app/(shop)/cart/page.tsx
@@ -18,26 +18,29 @@ export default function() {
             {/* Carrito */}
             <div className="flex flex-col mt-5">
               <span className="text-xl">Agregar mas items</span>
-              <Link href="/" className="mb-5">
+              <Link href="/" className="underline mb-5">
                 Continua comprando
               </Link >
-            </div>
 
             {/* Items */}
             {
               productsInCart.map(product => (
-                <div key={product.slug} className="flex">
+                <div key={product.slug} className="flex mb-5">
                     <Image 
                       src={`/products/${product.images[0]}`}
                       width={100}
                       height={100}
+                      style={{
+                        width: '100px',
+                        height: '100px'
+                      }}
                       alt={product.title}
                       className="mr-5 rounded"
                     />
                   <div>
                     <p>{product.title}</p>
                     <p>${product.price}</p>
-                    <QuantitySelector quantity={ 3 }/>
+                    <QuantitySelector quantity={ 1 }/>
                     <button className="underline mt-3">
                       Remover
                     </button>
@@ -45,9 +48,31 @@ export default function() {
                 </div>
               ))
             }
+            </div>
+            {/* Checkout - Resumen de la compra*/}
+            <div className="bg-white rounded-xl shadow-xl p-7">
+              <h2 className="text-2xl mb-2">Resumen de orden</h2>
+              <div className="grid grid-cols-2">
+                <span>No. Productos</span>
+                <span className="text-right">3 Articulos</span>
 
+                <span>Subtotal</span>
+                <span className="text-right">$100</span>
 
-            {/* Checkout */}
+                <span>Impuestos (15%)</span>
+                <span className="text-right">$100</span>
+
+                <span className="text-2xl mt-5">Total: (15%)</span>
+                <span className="text-right mt-5 text-2xl">$100</span>
+              </div>
+              <div className="mt-5 mb-2 w-full">
+                <Link 
+                  className="flex btn-primary justify-center"
+                  href="/checkout/address">
+                  Checkout
+                </Link>
+              </div>
+            </div>
         </div>
       </div>
     </div>
