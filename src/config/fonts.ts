430649327d74d452f859b93eb96726b61e187fbd
Fernando <jesusfgm45@gmail.com>
2024-06-11 12:59:35 -0600

    Login - Registro y Auth Layout

--- src/config/fonts.ts
+++ src/config/fonts.ts
@@ -3,5 +3,6 @@ import { Inter, Montserrat_Alternates } from "next/font/google";
 export const inter = Inter({ subsets: ["latin"] });
 export const titleFont = Montserrat_Alternates({ 
     subsets: ["latin"],
-    weight: ['500', '700']
+    weight: ['400', '700']
+    // weight: ['400']
 });
