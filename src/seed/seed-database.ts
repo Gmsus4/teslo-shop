0e7d50d87d5b6fe56581e8e9c0232f5c9e3e1de2
Fernando <jesusfgm45@gmail.com>
2024-06-11 16:23:07 -0600

    Prisma Client - Borrar contenido de tablas

--- src/seed/seed-database.ts
+++ src/seed/seed-database.ts
@@ -1,7 +1,20 @@
 import { initialData } from "./seed";
+import prisma from '../lib/prisma';
 
 async function main(){
-    console.log(initialData);
+
+    //1. Borrar registros previos
+    await Promise.all([
+        prisma.productImage.deleteMany(),
+        prisma.product.deleteMany(),
+        prisma.category.deleteMany(),
+    ]);
+
+    // await prisma.productImage.deleteMany();
+    // await prisma.product.deleteMany();
+    // await prisma.category.deleteMany();
+    
+    // console.log(initialData);
     console.log('Seed ejecutado correctamente')
 }
 
