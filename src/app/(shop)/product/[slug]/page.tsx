62aa29a66d3629a93152753df1a1bc449c609fb9
Fernando <jesusfgm45@gmail.com>
2024-06-13 13:08:53 -0600

    Probar enlaces para compartir

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -3,6 +3,7 @@ export const revalidate = 604800; //7 dias
 import { getProductBySlug } from "@/actions";
 import { ProductMobileSlideshow, ProductSlideshow, QuantitySelector, SizeSelector, StockLabel } from "@/components";
 import { titleFont } from "@/config/fonts";
+import { Metadata, ResolvingMetadata } from "next";
 
 import { notFound } from "next/navigation";
 
@@ -11,6 +12,33 @@ interface Props{
     slug: string;
   }
 }
+
+export async function generateMetadata(
+  { params }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  // read route params
+  const slug = params.slug
+ 
+  // fetch data
+  const product = await getProductBySlug(slug);
+ 
+  // optionally access and extend (rather than replace) parent metadata
+  //const previousImages = (await parent).openGraph?.images || []
+ 
+  return {
+    title: product?.title ?? 'Producto no encontrado',
+    description: product?.description ?? '',
+    openGraph: {
+      title: product?.title ?? 'Producto no encontrado',
+      description: product?.description ?? '',
+      // images: [],
+      images: [`/product/${ product?.images[1] }`]
+
+    },
+  }
+}
+
 export default async function ProductBySlugPage({ params }:Props) {
   const { slug } = params;
   const product = await getProductBySlug(slug)
