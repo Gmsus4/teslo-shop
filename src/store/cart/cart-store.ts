e1246b18b7047cbd7b215f69ca43a67929eba0c4
Fernando <jesusfgm45@gmail.com>
2024-06-14 10:10:11 -0600

    Hacer persistente el carrito de compras y mostrar el numero de elementos en el carrito

--- src/store/cart/cart-store.ts
+++ src/store/cart/cart-store.ts
@@ -1,45 +1,60 @@
 import type { CartProduct } from "@/interfaces";
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-interface State{
-    cart: CartProduct[]
+interface State {
+  cart: CartProduct[];
 
+  getTotalItems: () => number;
 
-    addProductToCart: (product: CartProduct) => void
-    //updateProductQuantity
-    //removeProduct
+  addProductToCart: (product: CartProduct) => void;
+  //updateProductQuantity
+  //removeProduct
 }
 
 export const useCartStore = create<State>()(
+  persist(
     (set, get) => ({
-        cart: [],
+      cart: [],
 
-        //Methods
-        addProductToCart: (product: CartProduct) => {
-            const { cart } = get();
+      //Methods
+      getTotalItems: () => {
+        const { cart } = get();
+        return cart.reduce((total, item) => total + item.quantity, 0);
+        //previusValue = El valor inicial que se ira acomulando
+        //currentValue = El valor iterado
+        //return cart.reduce((previusValue, currentValue) => previusValue + currentValue.quantity, 0);
+      },
 
-            //1. Revisar si el producto existe en el carrito con la talla seleccionada
-            //Action: Agregar al carrito, y si este ya existe con el mismo id y size, entonces lo acutaliza con el updateCartProducts
-            const productInCart = cart.some(
-                (item) => item.id === product.id && item.size === product.size
-            )
+      addProductToCart: (product: CartProduct) => {
+        const { cart } = get();
 
-            if( !productInCart){
-                set({ cart: [ ...cart, product ]});
-                return;
-            }
+        //1. Revisar si el producto existe en el carrito con la talla seleccionada
+        //Action: Agregar al carrito, y si este ya existe con el mismo id y size, entonces lo acutaliza con el updateCartProducts
+        const productInCart = cart.some(
+          (item) => item.id === product.id && item.size === product.size
+        );
 
-            //2. Se que el producto existe por talla | 
-            //Action: Agrega mas cantidad si el producto tiene la misma id y el mismo tamaño
-            const updateCartProducts = cart.map((item) => {
-                if(item.id  === product.id && item.size === product.size){
-                    return { ...item, quantity: item.quantity + product.quantity}
-                }
-
-                return item;
-            })
-
-            set({ cart: updateCartProducts })
+        if (!productInCart) {
+          set({ cart: [...cart, product] });
+          return;
         }
-    })
-)
+
+        //2. Se que el producto existe por talla |
+        //Action: Agrega mas cantidad si el producto tiene la misma id y el mismo tamaño
+        const updateCartProducts = cart.map((item) => {
+          if (item.id === product.id && item.size === product.size) {
+            return { ...item, quantity: item.quantity + product.quantity };
+          }
+
+          return item;
+        });
+
+        set({ cart: updateCartProducts });
+      },
+    }),
+    {
+      name: "shoping-cart",
+    }
+  )
+);
