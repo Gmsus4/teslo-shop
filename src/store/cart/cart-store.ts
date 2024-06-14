8f1bd21a3647bf72fd84aba3f03418a8058c5ddd
Fernando <jesusfgm45@gmail.com>
2024-06-14 08:46:07 -0600

    se agrego algunos comentarios

--- src/store/cart/cart-store.ts
+++ src/store/cart/cart-store.ts
@@ -19,6 +19,7 @@ export const useCartStore = create<State>()(
             const { cart } = get();
 
             //1. Revisar si el producto existe en el carrito con la talla seleccionada
+            //Action: Agregar al carrito, y si este ya existe con el mismo id y size, entonces lo acutaliza con el updateCartProducts
             const productInCart = cart.some(
                 (item) => item.id === product.id && item.size === product.size
             )
@@ -28,7 +29,8 @@ export const useCartStore = create<State>()(
                 return;
             }
 
-            //2. Se que el producto existe por talla
+            //2. Se que el producto existe por talla | 
+            //Action: Agrega mas cantidad si el producto tiene la misma id y el mismo tamaño
             const updateCartProducts = cart.map((item) => {
                 if(item.id  === product.id && item.size === product.size){
                     return { ...item, quantity: item.quantity + product.quantity}
