4eae260737e741ab6a6b1302fccec23ba55cf173
Fernando <jesusfgm45@gmail.com>
2024-06-14 08:13:18 -0600

    Store - addProductToCart

--- src/app/(shop)/product/[slug]/ui/AddToCart.tsx
+++ src/app/(shop)/product/[slug]/ui/AddToCart.tsx
@@ -16,7 +16,8 @@ export const AddToCart = ({ product }:Props) => {
   const addToCart = () => {
     setPosted(true);
     if(!size) return;
-    console.log({size, quantity})
+    console.log({size, quantity, product});
+    //Todo: addToCart
   }
 
   return (
--- src/store/cart/cart-store.ts
+++ src/store/cart/cart-store.ts
@@ -5,13 +5,39 @@ interface State{
     cart: CartProduct[]
 
 
-    //addProductToCart
+    addProductToCart: (product: CartProduct) => void
     //updateProductQuantity
     //removeProduct
 }
 
 export const useCartStore = create<State>()(
-    (set) => ({
-        cart: []
+    (set, get) => ({
+        cart: [],
+
+        //Methods
+        addProductToCart: (product: CartProduct) => {
+            const { cart } = get();
+
+            //1. Revisar si el producto existe en el carrito con la talla seleccionada
+            const productInCart = cart.some(
+                (item) => item.id === product.id && item.size === product.size
+            )
+
+            if( !productInCart){
+                set({ cart: [ ...cart, product ]});
+                return;
+            }
+
+            //2. Se que el producto existe por talla
+            const updateCartProducts = cart.map((item) => {
+                if(item.id  === product.id && item.size === product.size){
+                    return { ...item, quantity: item.quantity + product.quantity}
+                }
+
+                return item;
+            })
+
+            set({ cart: updateCartProducts })
+        }
     })
 )
