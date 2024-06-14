8a563955d85dcca0961b01c7fce6a1abb28924e4
Fernando <jesusfgm45@gmail.com>
2024-06-14 09:17:35 -0600

    Agregar producto al carrito de compras

--- src/app/(shop)/product/[slug]/ui/AddToCart.tsx
+++ src/app/(shop)/product/[slug]/ui/AddToCart.tsx
@@ -1,13 +1,15 @@
 'use client'
 
 import { QuantitySelector, SizeSelector } from "@/components"
-import { Product, Size } from "@/interfaces";
+import type { CartProduct, Product, Size } from "@/interfaces";
+import { useCartStore } from "@/store";
 import { useState } from "react";
 
 interface Props{
   product: Product;
 }
 export const AddToCart = ({ product }:Props) => {
+  const addProductToCart = useCartStore(state => state.addProductToCart)
 
   const [size, setSize] = useState<Size | undefined>();
   const [quantity, setQuantity] = useState<number>(1);
@@ -16,8 +18,20 @@ export const AddToCart = ({ product }:Props) => {
   const addToCart = () => {
     setPosted(true);
     if(!size) return;
-    console.log({size, quantity, product});
-    //Todo: addToCart
+    
+    const cartProduct: CartProduct = {
+      id: product.id,
+      slug: product.slug,
+      title: product.title,
+      price: product.price,
+      quantity: quantity,
+      size: size,
+      image: product.images[0],
+    }
+    addProductToCart(cartProduct);
+    setPosted(false);
+    setQuantity(1);
+    setSize(undefined);
   }
 
   return (
