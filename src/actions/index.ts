0e1861ddede28bc96b7caacf0d3b1dd9d09ee6f1
Fernando <jesusfgm45@gmail.com>
2024-06-15 12:54:45 -0600

    Conectar Login Form con NextAuth - Error -  CallbackRouteError

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,3 +1,4 @@
 export { getProductBySlug } from './product/get-product-by-slug'
 export { getPaginatedProductWithImages } from './product/product-pagination'
 export { getStockBySlug } from './product/get-stock-by-slug'
+export * from './auth/login'
--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,5 +1,4 @@
-import type { NextAuthConfig } from 'next-auth';
-import NextAuth from 'next-auth';
+import NextAuth, { type NextAuthConfig } from 'next-auth';
 import Credentials from 'next-auth/providers/credentials';
 import { z } from 'zod';
  
@@ -11,21 +10,27 @@ export const authConfig: NextAuthConfig = {
     providers: [
         Credentials({
             async authorize(credentials) {
+      
               const parsedCredentials = z
                 .object({ email: z.string().email(), password: z.string().min(6) })
                 .safeParse(credentials);
-
-                if(!parsedCredentials.success) return null;
-
+      
+                if ( !parsedCredentials.success ) return null;
+      
                 const { email, password } = parsedCredentials.data;
-                console.log({email, password})
 
-                //Buscar el correo
+                console.log('AuthConfig.ts');
+                console.log({email, password});
+      
+      
+                // Buscar el correo
 
-                //Comparar contraseñas
-
-                //Regrear el usuario
-
+      
+                // Comparar las contraseñas
+      
+      
+                // Regresar el usuario sin el password
+      
                 return null;
             },
           }),
