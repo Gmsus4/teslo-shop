2cdba4ee3aa82cb821c9ff450d900727aaa8da7f
Fernando <jesusfgm45@gmail.com>
2024-06-16 21:27:37 -0600

    Integracion de validacion del formulario con yup

--- src/app/auth/new-account/ui/RegisterForm.tsx
+++ src/app/auth/new-account/ui/RegisterForm.tsx
@@ -1,51 +1,148 @@
-'use client'
+"use client";
 
+import clsx from "clsx";
 import Link from "next/link";
 import { SubmitHandler, useForm } from "react-hook-form";
 
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
 type FormInputs = {
-    name: string
-    email: string
-    password: string
-}
+  name: string;
+  email: string;
+  password: string;
+};
+
+const schema = yup.object().shape({
+  name: yup
+    .string()
+    .min(3, "El nombre debe tener al menos 3 caracteres")
+    .max(50, "El nombre es muy largo, debe tener menos de 50 caracteres")
+    .required("El nombre es obligatorio"),
+  email: yup
+    .string()
+    .email("El correo electrónico no es válido")
+    .required("El correo electrónico es obligatorio"),
+  password: yup
+    .string()
+    .min(8, "La contraseña es muy corta, debe tener al menos 8 caracteres")
+    .required("La contraseña es obligatoria"),
+});
 
 export const RegisterForm = () => {
-    const { register, handleSubmit, formState: {errors} } = useForm<FormInputs>();
+  // const { register, handleSubmit, formState: {errors} } = useForm<FormInputs>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm<FormInputs>({
+    resolver: yupResolver(schema),
+  });
 
-    const onSubmit: SubmitHandler<FormInputs> = async(data) => {
-        const { name, email, password } = data;
-        console.log({ name, email, password });
+  const onSubmit: SubmitHandler<FormInputs> = async (data) => {
+    const { name, email, password } = data;
+    console.log({ name, email, password });
 
-        //Server action
-    }
+    //Server action
+  };
   return (
-    <form onSubmit={handleSubmit( onSubmit )} className="flex flex-col">
-        {
-            errors.name?.type === 'required' && (
-                <span className="text-red-500">* El nombre es obligatorio</span>
-            )
-        }
-      <label htmlFor="email">Nombre completo</label>
-      <input
-        className="px-5 py-2 border bg-gray-200 rounded mb-5"
-        type="text"
-        autoFocus
-        { ...register('name', { required: true } ) }
-      />
+    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col">
+      <div className="mb-5">
+        <label
+          htmlFor="name"
+          className={clsx(
+            "block mb-2 text-sm font-medium text-gray-900 required",
+            {
+              "text-red-600": !!errors.name?.message,
+              // "text-green-600": isValid
+            }
+          )}
+        >
+          Nombre completo
+        </label>
+        <input
+          className={clsx("px-5 py-2 border bg-gray-200 rounded w-full", {
+            "bg-red-50 border border-red-500 text-red-900 placeholder-red-700 focus:ring-red-500 focus:border-red-500":
+              !!errors.name?.message,
+            "bg-green-50 border border-green-500 text-green-900 placeholder-green-700 focus:ring-green-500 focus:border-green-500":
+              isValid,
+          })}
+          type="text"
+          autoFocus
+          {...register("name", { required: true, minLength: 3 })}
+        />
+        {errors.name?.message && (
+          <p className="mt-2 text-xs text-red-600 dark:text-red-500">
+            <span className="font-medium">Oops! </span>
+            {errors.name.message}
+          </p>
+        )}
+      </div>
 
-      <label htmlFor="email">Correo electrónico</label>
-      <input
-        className="px-5 py-2 border bg-gray-200 rounded mb-5"
-        type="email"
-        { ...register('email', { required: true, pattern: /^(([^<>()\[\]\\.,;:\s@”]+(\.[^<>()\[\]\\.,;:\s@”]+)*)|(“.+”))@((\[[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}])|(([a-zA-Z\-0–9]+\.)+[a-zA-Z]{2,}))$/ } ) }
-      />
+      <div className="mb-5">
+        <label
+          htmlFor="email"
+          className={clsx(
+            "block mb-2 text-sm font-medium text-gray-900 required",
+            {
+              "text-red-600": !!errors.email?.message,
+              // "text-green-600": isValid
+            }
+          )}
+        >
+          Correo electrónico
+        </label>
+        <input
+          className={clsx("px-5 py-2 border bg-gray-200 rounded w-full", {
+            "bg-red-50 border border-red-500 text-red-900 placeholder-red-700 focus:ring-red-500 focus:border-red-500":
+              !!errors.email?.message,
+            "bg-green-50 border border-green-500 text-green-900 placeholder-green-700 focus:ring-green-500 focus:border-green-500":
+              isValid,
+          })}
+          type="email"
+          {...register("email", {
+            required: true,
+            pattern:
+              /^(([^<>()\[\]\\.,;:\s@”]+(\.[^<>()\[\]\\.,;:\s@”]+)*)|(“.+”))@((\[[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}])|(([a-zA-Z\-0–9]+\.)+[a-zA-Z]{2,}))$/,
+          })}
+        />
+        {errors.email?.message && (
+          <p className="mt-2 text-xs text-red-600 dark:text-red-500">
+            <span className="font-medium">Oops!</span> {errors.email.message}
+          </p>
+        )}
+      </div>
 
-      <label htmlFor="password">Contraseña</label>
-      <input
-        className="px-5 py-2 border bg-gray-200 rounded mb-5"
-        type="password"
-        { ...register('password', { required: true }) }
-      />
+      <div className="mb-5">
+        <label
+          htmlFor="password"
+          className={clsx(
+            "block mb-2 text-sm font-medium text-gray-900 required w-full",
+            {
+              "text-red-600": !!errors.password?.message,
+              // "text-green-600": isValid
+            }
+          )}
+        >
+          Contraseña
+        </label>
+        <input
+          className={clsx("px-5 py-2 border bg-gray-200 rounded w-full", {
+            "bg-red-50 border border-red-500 text-red-900 placeholder-red-700 focus:ring-red-500 focus:border-red-500":
+              !!errors.password?.message,
+            "bg-green-50 border border-green-500 text-green-900 placeholder-green-700 focus:ring-green-500 focus:border-green-500":
+              isValid,
+          })}
+          type="password"
+          {...register("password", { required: true, minLength: 6 })}
+        />
+        {errors.password?.message && (
+          <p className="mt-2 text-xs text-red-600">
+            <span className="font-medium">Oops! </span>
+            {errors.password.message}
+          </p>
+        )}
+      </div>
 
       <button className="btn-primary">Crear cuenta</button>
 
