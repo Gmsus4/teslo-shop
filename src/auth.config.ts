ce1f69f142973bebb3153aed13bdf02a38b6a97d
Fernando <jesusfgm45@gmail.com>
2024-06-17 22:49:08 -0600

    Middleware

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -3,6 +3,21 @@ import Credentials from 'next-auth/providers/credentials';
 import { z } from 'zod';
 import prisma from './lib/prisma';
 import bcryptjs from 'bcryptjs';
+
+const authenticatedRoutes = [
+  //"/cart",
+  "empty",
+  "/checkout",
+  "/profile",
+  "/orders",
+  "/admin",
+];
+
+const isOnAuthenticatedRoutes = (onRoute: string) => {
+  return authenticatedRoutes.some((authRoutes) => //Itera por cada elemento del array y si al menos uno coincide, esto retorna true
+    onRoute.startsWith(authRoutes)//Compara la iteracion de los elementos del array con la ruta en la que esta el usuario, si estas coninciden retorna true, si no coinciden retorna false
+  );
+};
  
 export const authConfig: NextAuthConfig = {
     pages: {
@@ -11,6 +26,17 @@ export const authConfig: NextAuthConfig = {
     },
 
     callbacks: {
+        authorized({ auth, request: { nextUrl } }) {
+            console.log({auth})
+            const isLoggedIn = !!auth?.user; //Saber si el usuario incio sesion
+
+            if (isOnAuthenticatedRoutes(nextUrl.pathname)) { //Funcion que retorna true si la ruta es una ruta protegida solo para usuarios que inicien sesion
+                if (isLoggedIn) return true; //Ahora que sabemos que necesita estar autenticado, si es asi, tiene acceso a la ruta
+                return false; // Pero si no, entonces lo redirecciona a: auth/login - Redirect unauthenticated users to login page
+            }
+            return true; //Si la ruta no cae en ninguna ruta autorizada, entonces no hace nada, es una ruta para cualquier usuario.
+        },
+
         jwt({token, user}){
             if(user){
                 token.data = user;
