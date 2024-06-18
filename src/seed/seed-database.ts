a5772bc441921587d0873469fab6e9827714ed6a
Fernando <jesusfgm45@gmail.com>
2024-06-18 09:31:31 -0600

    Seed countries

--- src/seed/seed-database.ts
+++ src/seed/seed-database.ts
@@ -1,9 +1,11 @@
 import { initialData } from "./seed";
 import prisma from '../lib/prisma';
+import { countries } from "./seed-countries";
 
 async function main(){
     //Borrar registros previos
     await prisma.productImage.deleteMany();
+    await prisma.country.deleteMany();
     await prisma.product.deleteMany();
     await prisma.category.deleteMany();
     await prisma.user.deleteMany();
@@ -12,6 +14,11 @@ async function main(){
     //Usuarios
     await prisma.user.createMany({
         data: users
+    });
+
+    // Countries
+    await prisma.country.createMany({
+        data: countries
     });
 
     // Categorias
