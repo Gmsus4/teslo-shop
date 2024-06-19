fd5900b520d5e484bebd8e70a0126d42a95ef6a1
Fernando <jesusfgm45@gmail.com>
2024-06-19 14:54:40 -0600

    Pantalla de confirmacion parte 1

--- src/app/(shop)/checkout/(checkout)/page.tsx
+++ src/app/(shop)/checkout/(checkout)/page.tsx
@@ -1,20 +1,6 @@
-import { QuantitySelector, Title } from "@/components";
-import { initialData } from "@/seed/seed";
-import Image from "next/image";
+import { Title } from "@/components";
 import Link from "next/link";
-
-const productsInCart = [
-  initialData.products[0],
-  initialData.products[1],
-  initialData.products[2],
-  // initialData.products[3],
-  // initialData.products[4],
-  // initialData.products[5],
-  // initialData.products[6],
-  // initialData.products[7],
-  // initialData.products[8],
-  // initialData.products[9],
-]
+import { ProductsInCart } from "./ui/ProductsInCart";
 
 export default function CheckoutPage() {
   return (
@@ -29,31 +15,7 @@ export default function CheckoutPage() {
           </Link >
 
         {/* Items */}
-        {
-          productsInCart.map(product => (
-            <div key={product.slug} className="flex mb-5">
-                <Image 
-                  src={`/products/${product.images[0]}`}
-                  width={100}
-                  height={100}
-                  style={{
-                    width: '100px',
-                    height: '100px'
-                  }}
-                  alt={product.title}
-                  className="mr-5 rounded"
-                />
-              <div>
-                <p>{product.title}</p>
-                <p>${product.price} x 3</p>
-                <p className="font-bold">Subtotal: ${product.price * 3}</p>
-                <button className="underline mt-3">
-                  Remover
-                </button>
-              </div>
-            </div>
-          ))
-        }
+        <ProductsInCart />
         </div>
       </div>
       {/* Checkout - Resumen de la compra*/}
